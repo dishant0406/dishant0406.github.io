1a3252636f03fc2063cc4aa03325409fe6c5b96d
Dishant Sharma <dishu5570@gmail.com>
2022-03-24 10:54:02 +0530

    Updates

---
+++ static/js/10.38da202d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpportfolio-app"]=this["webpackJsonpportfolio-app"]||[]).push([[10],{89:function(t,e,p){"use strict";p.r(e);p(0),p(91);var a=p(1);e.default=function(t){var e=t.title;return Object(a.jsx)("div",{className:"header",children:e})}}}]);
+//# sourceMappingURL=10.38da202d.chunk.js.map
